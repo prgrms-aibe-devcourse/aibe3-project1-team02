3bc846a5332b1aa491f973600bcb2b92e534aa10
seungin <tmddls91@inha.edu>
2025-07-17 11:27:14 +0900

    [fix] 401 오류 해결을 위해 코드 되돌리기 진행 (#7)

--- src/lib/supabase-server.ts
+++
@@ -1,6 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-export const createServerClient = () => {
-    return createServerComponentClient({ cookies })
-}
