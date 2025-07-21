e94d778cccdbe9f7b4584f7bd40c1cb475aa82ef
Jeongho Kim <kimjungho.kr@gmail.com>
2025-07-21 16:44:14 +0900

    [design] 헤더 여행후기-> 커뮤니티로 이름 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -80,7 +80,7 @@ export default function Header() {
                             href="/community"
                             className="text-gray-700 hover:text-blue-600 transition-colors cursor-pointer"
                         >
-                            여행후기
+                            커뮤니티
                         </Link>
                     </nav>
 
